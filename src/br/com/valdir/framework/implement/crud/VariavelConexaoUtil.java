f9ecdcf73d91946b5b00e3243129c83029521fce
Valdir de Camargo <valdir.camargo@yahoo.com.br>
2018-02-15 17:23:34 -0200

    Conf classe de pesquisa

---
+++ src/br/com/valdir/framework/implement/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.valdir.framework.implement.crud;
+
+/**
+ * Nome do caminho do JNDi datasource Tomcat
+ * @author Valdir De Camargo
+ *
+ */
+public class VariavelConexaoUtil {
+	
+	// VARIAVEL QUE RECUPERA O CAMINHO DO BANCO
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/env/jdbc/datasource";
+ 
+}

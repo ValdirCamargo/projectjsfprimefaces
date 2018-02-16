26a54b3f293b25c91048ae19d6ca5c980f05d30f
Valdir de Camargo <valdir.camargo@yahoo.com.br>
2018-02-16 12:21:59 -0200

    Classes de Mensagens Jsf e PrimeFaces Criadas

---
+++ src/br/com/valdir/project/util/all/Messagens.java
@@ -0,0 +1,75 @@
+package br.com.valdir.project.util.all;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public abstract class Messagens extends FacesContext implements Serializable  {
+
+	private static final long serialVersionUID = 1L;
+	
+	public Messagens() {
+	}
+	
+	public static FacesContext getFacesContext(){
+		return FacesContext.getCurrentInstance();
+	}
+	
+	public static boolean facesContextValido(){
+		return getFacesContext() != null;
+	}
+	
+	public static void erroNaOperacao(){
+		if(facesContextValido()){
+			msgSeverityFatal(Constante.ERRO_NA_OPERACAO);
+		}
+	}
+	
+	public static void msg(String msg){
+		if(facesContextValido()){
+			getFacesContext().addMessage("msg", new FacesMessage(msg));
+		}
+	}
+	
+	public static void responseOperation(EstatusPersistencia estatusPersistencia){
+		if (estatusPersistencia != null 
+				&& estatusPersistencia.equals(EstatusPersistencia.SUCESSO)) {
+			sucesso();
+		}else if(estatusPersistencia != null
+				&& estatusPersistencia.equals(EstatusPersistencia.OBJETO_REFERENCIADO)){
+			msgSeverityFatal(EstatusPersistencia.OBJETO_REFERENCIADO.toString());
+		}else{
+			erroNaOperacao();
+		}
+	}
+	
+	public static void sucesso(){
+		msgSeverityInfo(Constante.OPERACAO_REALIZADA_COM_SUCESSO);
+	}
+	
+	public static void msgSeverityInfo(String msg){
+		if(facesContextValido()){
+			getFacesContext().addMessage("msg", new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg));
+		}
+	}
+	
+	public static void msgSeverityError(String msg){
+		if(facesContextValido()){
+			getFacesContext().addMessage("msg", new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg));
+		}
+	}
+	
+	public static void msgSeverityWarn(String msg){
+		if(facesContextValido()){
+			getFacesContext().addMessage("msg", new FacesMessage(FacesMessage.SEVERITY_WARN, msg, msg));
+		}
+	}
+	
+	public static void msgSeverityFatal(String msg){
+		if(facesContextValido()){
+			getFacesContext().addMessage("msg", new FacesMessage(FacesMessage.SEVERITY_FATAL, msg, msg));
+		}
+	}
+
+}
